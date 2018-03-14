1fc2873e4d5da2a9c7e7a0cf65f0896fd37e28c5
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2018-03-14 09:22:28 -0400

    IT'S TIME TO D-D-D-D-D-D-D-D-DUEL!!!!!

---
+++ Lab_4.1/src/Deck.java
@@ -0,0 +1,13 @@
+
+public class Deck 
+{
+	private static final int cards = 0;
+
+	public Deck(String[]Rank, String[]Suit, String[]Value)
+	{
+		for (int i = 0 ; i < cards.length ; i++)
+		{
+			undealt.add(new card(this.ranks))
+		}
+	}
+}
